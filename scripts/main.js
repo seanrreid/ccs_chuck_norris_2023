a762152d4fe79904b7b34a7d954aa918d978b13f
Sean R Reid <sean@torchcodelab.com>
2023-09-13 13:29:26 -0400

    Added fetch request, updating the DOM

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function () {
+    console.log('DOM IS READY');
+
+    const chuckQuote = document.querySelector('#chuckQuote');
+
+    fetch('https://api.chucknorris.io/jokes/random?category=dev')
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            showQuote(data.value, chuckQuote);
+            return data;
+        });
+});
+
+function showQuote(quote, element) {
+    setTimeout(() => {
+        element.innerText = quote;
+    }, 3000);
+}
